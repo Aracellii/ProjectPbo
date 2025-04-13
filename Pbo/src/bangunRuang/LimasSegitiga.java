1240de46c2df30c7acd96478e1685958fbc2cd97
Joestarya <aryarudysyahputra@gmail.com>
2025-04-13 15:27:13 +0700

    LIMAS

--- Pbo/src/bangunRuang/LimasSegitiga.java
+++ Pbo/src/bangunRuang/LimasSegitiga.java
@@ -1,38 +1,20 @@
 package bangunRuang;
 
-package bangunRuang;
-import bangunDatar.BangunDatar;
-
-public class Limas extends BangunRuang {
-    private BangunDatar alas;
+public class LimasSegitiga extends BangunRuang {
     private double tinggi;
     private double[] sisiTegak;
 
     // Constructor lengkap
-    public Limas(BangunDatar alas, double tinggi, double[] sisiTegak) {
-        this.alas = alas;
+    public LimasSegitiga( ) {
         this.tinggi = tinggi;
         this.sisiTegak = sisiTegak;
     }
 
     public void hitungVolume() {
-        // Volume = 1/3 × Luas Alas × Tinggi
-
-                double volume = (1.0 / 3.0) * p.getLuas() * tinggi;
-                System.out.println("Volume Limas: " + volume);
-
-            }
         
     }
 
-    @Override
     public void hitungLuasPermukaan() {
-       double luasSisiTegakTotal = 0;
-        for (double luas : sisiTegak) {
-            luasSisiTegakTotal += luas;
-        }
 
-        double luasPermukaan = alas.getLuas() + luasSisiTegakTotal;
-        System.out.println("Luas Permukaan Limas: " + luasPermukaan);
-    }
+}
 }
