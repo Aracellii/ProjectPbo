6015a064b0c382b09cda82d5c6ce18b8d79c7a90
Joestarya <aryarudysyahputra@gmail.com>
2025-04-14 07:58:01 +0700

    tembereng ni

--- Pbo/src/bangunRuang/TemberengBola.java
+++ Pbo/src/bangunRuang/TemberengBola.java
@@ -1,5 +1,26 @@
+package bangunRuang;
+import bangunDatar.Lingkaran;
 
-package BangunRuang;
-public class TemberengBola {
-    
+public class TemberengBola extends BangunRuang {
+    private Lingkaran lingkaran;
+    private double tinggi;
+
+    public TemberengBola(double jari, double tinggi) {
+        this.lingkaran = new Lingkaran(jari);
+        this.tinggi = tinggi;
+    }
+
+    @Override
+    public void hitungVolume() {
+        double r = lingkaran.getJari();
+        double volume = (1.0 / 6.0) * Math.PI * tinggi * tinggi * (3 * r - tinggi);
+        System.out.println("Volume Tembereng Bola: " + volume);
+    }
+
+    @Override
+    public void hitungLuasPermukaan() {
+        double r = lingkaran.getJari();
+        double luas = 2 * Math.PI * r * tinggi;
+        System.out.println("Luas Permukaan Tembereng Bola: " + luas);
+    }
 }
