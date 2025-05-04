f4387a4914bea8e3e3b65ba7e0bfbaa85f62506d
Luthfi Alaudin Rosyid <123230082@student.upnyk.ac.id>
2025-05-04 21:15:09 +0700

    plant

--- Pbo/src/bangunDatar/JuringLingkaran.java
+++ Pbo/src/bangunDatar/JuringLingkaran.java
@@ -1,22 +1,30 @@
 package bangunDatar;
 
 public class JuringLingkaran extends Lingkaran {
+<<<<<<< Updated upstream
     private final double sudut;
+=======
+    private double sudut;
+>>>>>>> Stashed changes
 
     public JuringLingkaran(double jari, double sudut) {
         super(jari);
         this.sudut = sudut;
     }
 
-    @Override
     public void hitungLuas() {
         double luasJuring = (sudut / 360) * super.getLuas();
         System.out.println("Luas Juring lingkaran: " + luasJuring);
     }
 
+<<<<<<< Updated upstream
     @Override
     public void hitungKeliling() {    
         double kelilingJuring = (sudut / 360) * super.getKeliling() + 2 * jari;
+=======
+    public void hitungKeliling() {    
+        double kelilingJuring = (sudut / 360) super.getKeliling() + 2 * jari;
+>>>>>>> Stashed changes
         System.out.println("Keliling Juring lingkaran: " + kelilingJuring);
     }
 }
