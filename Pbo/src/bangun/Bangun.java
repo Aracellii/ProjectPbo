ec178dbbac25355086bb9a9332acd7a613cbba8e
Luthfi Alaudin Rosyid <123230082@student.upnyk.ac.id>
2025-06-13 15:15:50 +0700

    O

--- Pbo/src/bangun/Bangun.java
+++ Pbo/src/bangun/Bangun.java
@@ -1,4 +1,8 @@
 package bangun;
 
 public interface Bangun {
+    public double hitungLuas();
+    public double hitungKeliling();
+    public double hitungVolume();
+    public double hitungLuasPermukaan();
 }
