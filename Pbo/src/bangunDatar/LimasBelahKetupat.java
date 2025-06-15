e864c6f071318ad149a361bf92a294afee02e25a
Alfa Rizky Hapsori <alfarizky1301@gmail.com>
2025-06-15 18:21:23 +0700

    add Limas Belah Ketupat

--- Pbo/src/bangunDatar/LimasBelahKetupat.java
+++ Pbo/src/bangunDatar/LimasBelahKetupat.java
@@ -2,12 +2,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+
 package bangunDatar;
 
-/**
- *
- * @author L E N O V O
- */
-public class LimasBelahKetupat {
-    
+public class LimasBelahKetupat extends BelahKetupat {
+    private double tinggiLimas;
+    private double tinggiSegitigaTegak;
+
+    public LimasBelahKetupat(double diagonalA, double diagonalB, double sisi,
+                             double tinggiLimas, double tinggiSegitigaTegak) {
+        super(diagonalA, diagonalB, sisi);
+        this.tinggiLimas = tinggiLimas;
+        this.tinggiSegitigaTegak = tinggiSegitigaTegak;
+    }
+
+    /**
+     * Volume limas = 1/3 × luas alas × tinggi limas
+     */
+    public double hitungVolume() {
+        return (1.0 / 3.0) * super.hitungLuas() * tinggiLimas;
+    }
+
+    /**
+     * Luas permukaan limas = luas alas + luas 4 segitiga selubung
+     * (setiap segitiga: 1/2 × sisi × tinggi segitiga)
+     */
+    public double hitungLuasPermukaan() {
+        double luasSelubung = 4 * (0.5 * sisi * tinggiSegitigaTegak);
+        return super.hitungLuas() + luasSelubung;
+    }
 }
