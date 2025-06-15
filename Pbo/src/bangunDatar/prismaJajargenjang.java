4c0667044f955b75ea878c369c63e9825104bbf7
chys <milathina@gmail.com>
2025-06-15 20:32:50 +0700

    prizm jajargenjg

--- Pbo/src/bangunDatar/prismaJajargenjang.java
+++ Pbo/src/bangunDatar/prismaJajargenjang.java
@@ -1,19 +1,55 @@
 package bangunDatar;
 
-public class PrismaJajargenjang extends JajarGenjang{
-    protected double tinggiPrisma;
+import java.util.Scanner;
+
+public class PrismaJajarGenjang extends JajarGenjang{
+    private double tinggiPrisma;
+    private double volume;
+    private double luasPermukaan;
         
-    public PrismaJajargenjang(double alas, double sisiMiring, double tinggi, double tinggiPrisma) {
+    public PrismaJajarGenjang(double alas, double sisiMiring, double tinggi, double tinggiPrisma) {
         super(alas, sisiMiring, tinggi);
         this.tinggiPrisma = tinggiPrisma;
     }
     
     private double hitungVolume(){
-        return hitungLuas() * tinggiPrisma;
+        volume = super.luas * tinggiPrisma;
+        return volume;
     }
     
     private double hitungLuasPermukaan(){
-        return (2 * hitungLuas()) + (hitungKeliling() * tinggiPrisma);
+        luasPermukaan = (2 * super.luas) + (super.keliling * tinggiPrisma);
+        return luasPermukaan;
     }
-         
+    
+    
+    public static class PrismaJajarGenjangRunnable implements Runnable {
+        private PrismaJajarGenjang prismaJajarGenjang;
+
+        @Override
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Masukkan alas jajar genjang: ");
+            double alas = scanner.nextDouble();
+
+            System.out.print("Masukkan sisi miring jajar genjang: ");
+            double sisiMiring = scanner.nextDouble();
+
+            System.out.print("Masukkan tinggi jajar genjang: ");
+            double tinggi = scanner.nextDouble();
+            
+            System.out.print("Masukkan tinggi prisma: ");
+            double tinggiPrisma = scanner.nextDouble();
+
+            prismaJajarGenjang = new PrismaJajarGenjang(alas, sisiMiring, tinggi, tinggiPrisma);
+
+            System.out.println("Volume prisma jajar genjang: " + prismaJajarGenjang.hitungVolume());
+            System.out.println("Luas Permukaan prisma jajar genjang: " + prismaJajarGenjang.hitungLuasPermukaan());
+        }
+
+        public PrismaJajarGenjang getPrismaJajarGenjang() {
+            return prismaJajarGenjang;
+        }
+    }
 }
