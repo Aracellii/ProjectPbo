cf5d70eeaf1497027befc7fd09b5462e484f04e4
chys <milathina@gmail.com>
2025-06-15 20:06:14 +0700

    prizma kupat

--- Pbo/src/bangunDatar/PrismaBelahKetupat.java
+++ Pbo/src/bangunDatar/PrismaBelahKetupat.java
@@ -1,5 +1,7 @@
 package bangunDatar;
 
+import java.util.Scanner;
+
 public class PrismaBelahKetupat extends BelahKetupat {
     private double tinggiPrisma;
     private double volume;
@@ -18,5 +20,36 @@ public class PrismaBelahKetupat extends BelahKetupat {
     public double hitungLuasPermukaan() {
         luasPermukaan = 2 * super.luas + super.keliling * tinggiPrisma;
         return luasPermukaan;
+    }
+    
+    
+    public static class PrismaBelahKetupatRunnable implements Runnable {
+        private PrismaBelahKetupat prismaBelahKetupat;
+
+        @Override
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Masukkan panjang diagonal A belah ketupat: ");
+            double diagonalA = scanner.nextDouble();
+
+            System.out.print("Masukkan panjang diagonal B belah ketupat: ");
+            double diagonalB = scanner.nextDouble();
+
+            System.out.print("Masukkan panjang sisi belah ketupat: ");
+            double sisi = scanner.nextDouble();
+            
+            System.out.print("Masukkan tinggi prisma: ");
+            double tinggiPrisma = scanner.nextDouble();
+
+            prismaBelahKetupat = new PrismaBelahKetupat(diagonalA, diagonalB, sisi, tinggiPrisma);
+
+            System.out.println("Luas belah ketupat: " + prismaBelahKetupat.hitungLuas());
+            System.out.println("Keliling belah ketupat: " + prismaBelahKetupat.hitungKeliling());
+        }
+
+        public PrismaBelahKetupat getPrismaBelahKetupat() {
+            return prismaBelahKetupat;
+        }
     }
 }
