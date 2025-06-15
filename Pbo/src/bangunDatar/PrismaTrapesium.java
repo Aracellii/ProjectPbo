70ab3dfd71d7d8e455767e5378a28af97ddc8c07
chys <milathina@gmail.com>
2025-06-15 19:55:15 +0700

    prizma trapez

--- Pbo/src/bangunDatar/PrismaTrapesium.java
+++ Pbo/src/bangunDatar/PrismaTrapesium.java
@@ -1,12 +1,14 @@
 package bangunDatar;
 
+import java.util.Scanner;
+
 public class PrismaTrapesium extends Trapesium {
     private double tinggiPrisma;
     private double volume;
     private double luasPermukaan;
 
-    public PrismaTrapesium(double sisiAtas, double sisiBawah, double sisiKiri, double sisiKanan, double tinggiTrapesium, double tinggiPrisma) {
-        super(sisiAtas, sisiBawah, sisiKiri, sisiKanan, tinggiTrapesium);
+    public PrismaTrapesium(double sisiAtas, double sisiBawah, double sisiMiring, double tinggi, double tinggiPrisma) {
+        super(sisiAtas, sisiBawah, sisiMiring, tinggi);
         this.tinggiPrisma = tinggiPrisma;
     }
 
@@ -18,5 +20,38 @@ public class PrismaTrapesium extends Trapesium {
     public double hitungLuasPermukaan() {
         luasPermukaan = 2 * super.luas + super.keliling * tinggiPrisma;
         return luasPermukaan;
+    }
+    
+    public static class PrismaTrapesiumRunnable implements Runnable {
+        private PrismaTrapesium prismaTrapesium;
+
+        @Override
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Masukkan panjang sisi atas trapesium: ");
+            double sisiAtas = scanner.nextDouble();
+
+            System.out.print("Masukkan panjang sisi bawah trapesium: ");
+            double sisiBawah = scanner.nextDouble();
+
+            System.out.print("Masukkan panjang sisi kiri trapesium: ");
+            double sisiMiring = scanner.nextDouble();
+
+            System.out.print("Masukkan tinggi trapesium: ");
+            double tinggi = scanner.nextDouble();
+            
+            System.out.print("Masukkan tinggi prisma: ");
+            double tinggiPrisma = scanner.nextDouble();
+
+            prismaTrapesium = new PrismaTrapesium (sisiAtas, sisiBawah, sisiMiring, tinggi, tinggiPrisma);
+
+            System.out.println("Luas trapesium: " + prismaTrapesium.hitungLuas());
+            System.out.println("Keliling trapesium: " + prismaTrapesium.hitungKeliling());
+        }
+
+        public PrismaTrapesium getPrismaTrapesium() {
+            return prismaTrapesium;
+        }
     }
 }
