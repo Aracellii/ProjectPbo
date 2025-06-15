f3f5ceea09d0ce3eefb619ecd24ca169498e0574
chys <milathina@gmail.com>
2025-06-15 17:45:50 +0700

    limas layang, prisma trapesium credit: fiyya

--- Pbo/src/bangunDatar/LimasLayangLayang.java
+++ Pbo/src/bangunDatar/LimasLayangLayang.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bangunDatar;
 
-/**
- *
- * @author L E N O V O
- */
-public class LimasLayangLayang {
-    
+public class LimasLayangLayang extends LayangLayang {
+    private double tinggiLimas;
+    private double volume;
+    private double luasPermukaan;
+
+    public LimasLayangLayang(double diagonalA, double diagonalB, double sisiPendek, double sisiPanjang, double tinggiLimas) {
+        super(diagonalA, diagonalB, sisiPendek, sisiPanjang);
+        this.tinggiLimas = tinggiLimas;
+    }
+
+    public double hitungVolume() {
+        volume = (1.0 / 3.0) * super.luas * tinggiLimas;
+        return volume;
+    }
+
+    public double hitungLuasPermukaan() {
+        double sisiTegakPendek = Math.sqrt((tinggiLimas * tinggiLimas)  + (super.sisiPendek/2.0) * (super.sisiPendek/2.0));
+        double sisiTegakPanjang = Math.sqrt((tinggiLimas * tinggiLimas) + (super.sisiPanjang/2.0) * (super.sisiPendek/2.0));
+
+        double luasSisiPendek = 2 * (0.5 * super.sisiPendek * sisiTegakPendek);
+        double luasSisiPanjang = 2 * (0.5 * super.sisiPanjang * sisiTegakPanjang);
+
+        luasPermukaan = super.luas + luasSisiPendek + luasSisiPanjang;
+        return luasPermukaan;
+    }
 }
--- Pbo/src/bangunDatar/PrismaTrapesium.java
+++ Pbo/src/bangunDatar/PrismaTrapesium.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bangunDatar;
 
-/**
- *
- * @author L E N O V O
- */
-public class PrismaTrapesium {
-    
+public class PrismaTrapesium extends Trapesium {
+    private double tinggiPrisma;
+    private double volume;
+    private double luasPermukaan;
+
+    public PrismaTrapesium(double sisiAtas, double sisiBawah, double sisiKiri, double sisiKanan, double tinggiTrapesium, double tinggiPrisma) {
+        super(sisiAtas, sisiBawah, sisiKiri, sisiKanan, tinggiTrapesium);
+        this.tinggiPrisma = tinggiPrisma;
+    }
+
+    public double hitungVolume() {
+        volume = super.luas * tinggiPrisma;
+        return volume;
+    }
+
+    public double hitungLuasPermukaan() {
+        luasPermukaan = 2 * super.luas + super.keliling * tinggiPrisma;
+        return luasPermukaan;
+    }
 }
