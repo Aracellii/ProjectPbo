06bb7e4e065da96a18eb0663c061d194ecd4417f
Alfa Rizky Hapsori <alfarizky1301@gmail.com>
2025-06-15 20:21:01 +0700

    PrismaSegitiga

--- Pbo/src/bangunDatar/PrismaSegiTiga.java
+++ Pbo/src/bangunDatar/PrismaSegiTiga.java
@@ -1,5 +1,7 @@
 package bangunDatar;
 
+import java.util.Scanner;
+
 public class PrismaSegiTiga extends Segitiga {
     private final double tinggiPrisma;
     private double volume;
@@ -7,17 +9,54 @@ public class PrismaSegiTiga extends Segitiga {
 
     public PrismaSegiTiga(double alas, double tinggi, double sisiA, double sisiB, double tinggiPrisma) {
         super(alas, tinggi, sisiA, sisiB);
-        this.tinggiPrisma = tinggiPrisma;   
+        this.tinggiPrisma = tinggiPrisma;
+        this.volume = hitungVolume();
+        this.luasPermukaan = hitungLuasPermukaan();
     }
 
     public double hitungVolume() {
-        volume = super.alas * tinggiPrisma;
+        volume = super.luas * tinggiPrisma;
         return volume;
     }
 
     public double hitungLuasPermukaan() {
-        luasPermukaan = 2 * super.alas + super.keliling * tinggiPrisma;
+        // Luas permukaan = 2 * luas alas + keliling alas * tinggi prisma
+        luasPermukaan = 2 * super.luas + super.keliling * tinggiPrisma;
         return luasPermukaan;
     }
-    
+
+    public static class PrismaSegiTigaRunnable implements Runnable {
+        private PrismaSegiTiga prisma;
+
+        @Override
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Masukkan alas segitiga: ");
+            double alas = scanner.nextDouble();
+
+            System.out.print("Masukkan tinggi segitiga: ");
+            double tinggiSegitiga = scanner.nextDouble();
+
+            System.out.print("Masukkan sisi A segitiga: ");
+            double sisiA = scanner.nextDouble();
+
+            System.out.print("Masukkan sisi B segitiga: ");
+            double sisiB = scanner.nextDouble();
+
+            System.out.print("Masukkan tinggi prisma: ");
+            double tinggiPrisma = scanner.nextDouble();
+
+            prisma = new PrismaSegiTiga(alas, tinggiSegitiga, sisiA, sisiB, tinggiPrisma);
+
+            System.out.println("Luas alas segitiga: " + prisma.hitungLuas());
+            System.out.println("Keliling alas segitiga: " + prisma.hitungKeliling());
+            System.out.println("Volume prisma segitiga: " + prisma.hitungVolume());
+            System.out.println("Luas permukaan prisma segitiga: " + prisma.hitungLuasPermukaan());
+        }
+
+        public PrismaSegiTiga getPrismaSegiTiga() {
+            return prisma;
+        }
+    }
 }
